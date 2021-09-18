563eeedb5513540c5ab60c61d09d3354aff8a2ea
Ali Eskandari <alieskandari3@gmail.com>
2021-09-18 15:10:56 -0700

    Added sign up page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { lazy, Suspense } from "react";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
+
 import Navbar from "./components/navbar";
 import Video from "./pages/video";
 import Channel from "./pages/channel";
+import SignIn from "./pages/sign-in";
+import ScrollToTop from "./components/scroll-to-top";
 
 const Home = lazy(() => import("./pages/home"));
 
@@ -12,12 +15,18 @@ function App() {
   return (
     <Container className="App">
       <Router>
-        <Navbar />
+        <ScrollToTop />
         <Suspense fallback={<p>Loading...</p>}>
           <Switch>
-            <Route exact path={ROUTES.HOME} component={Home} />
-            <Route exact path={ROUTES.VIDEO} component={Video} />
-            <Route exact path={ROUTES.CHANNEL} component={Channel} />
+            <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
+            <Route path={ROUTES.HOME}>
+              <Navbar />
+              <Switch>
+                <Route exact path={ROUTES.HOME} component={Home} />
+                <Route exact path={ROUTES.VIDEO} component={Video} />
+                <Route exact path={ROUTES.CHANNEL} component={Channel} />
+              </Switch>
+            </Route>
           </Switch>
         </Suspense>
       </Router>
--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,83 +1,67 @@
-import { useState, useContext, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import FirebaseContext from '../context/firebase';
-import * as ROUTES from '../constants/routes';
+import { useState, useContext, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import * as ROUTES from "../constants/routes";
 
-export default function Login() {
+export default function SignIn() {
   const history = useHistory();
-  const { firebase } = useContext(FirebaseContext);
 
-  const [emailAddress, setEmailAddress] = useState('');
-  const [password, setPassword] = useState('');
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [error, setError] = useState('');
-  const isInvalid = password === '' || emailAddress === '';
+  const [error, setError] = useState("");
+  const isInvalid = password === "" || emailAddress === "";
 
-  const handleLogin = async (event) => {
+  const handleSignIn = async (event) => {
     event.preventDefault();
 
     try {
-      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
-      history.push(ROUTES.DASHBOARD);
+      history.push(ROUTES.HOME);
     } catch (error) {
-      setEmailAddress('');
-      setPassword('');
+      setEmailAddress("");
+      setPassword("");
       setError(error.message);
     }
   };
 
   useEffect(() => {
-    document.title = 'Login - Instagram';
+    document.title = "Sign In - DomumGym";
   }, []);
 
   return (
-    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="flex w-3/5">
-        <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
-      </div>
-      <div className="flex flex-col w-2/5">
-        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
-          <h1 className="flex justify-center w-full">
-            <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
-          </h1>
+    <Row className="justify-content-center">
+      <Col xs={12} sm={5} className="border-3">
+        <h4 className="text-center">Sign In</h4>
 
-          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+        <Form onSubmit={handleSignIn}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="Email" />
+          </Form.Group>
 
-          <form onSubmit={handleLogin} method="POST">
-            <input
-              aria-label="Enter your email address"
-              type="text"
-              placeholder="Email address"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={({ target }) => setEmailAddress(target.value)}
-              value={emailAddress}
-            />
-            <input
-              aria-label="Enter your password"
-              type="password"
-              placeholder="Password"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={({ target }) => setPassword(target.value)}
-              value={password}
-            />
-            <button
-              disabled={isInvalid}
-              type="submit"
-              className={`bg-blue-medium text-white w-full rounded h-8 font-bold
-            ${isInvalid && 'opacity-50'}`}
-            >
-              Login
-            </button>
-          </form>
-        </div>
-        <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
-          <p className="text-sm">
-            Don't have an account?{` `}
-            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
-              Sign up
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Button
+            size="md"
+            className="w-100 mb-4"
+            variant="primary"
+            type="submit"
+          >
+            SIGN IN
+          </Button>
+
+          <div className="text-center mb-4">
+            <Link to="/sign-in" className="text-decoration-none">
+              Sign up for DomumGym
             </Link>
-          </p>
-        </div>
-      </div>
-    </div>
+          </div>
+
+          <Button size="md" className="w-100" variant="secondary" type="submit">
+            SIGN IN WITH GOOGLE
+          </Button>
+        </Form>
+      </Col>
+    </Row>
   );
+}
