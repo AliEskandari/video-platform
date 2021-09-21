5d80620d932d9811873326928c598aafbe34c6f8
Ali Eskandari <alieskandari3@gmail.com>
2021-09-20 19:25:37 -0700

    add 4 routes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import * as ROUTES from "./constants/routes";
 import Navbar from "./components/navbar";
 import Video from "./pages/video";
 import Channel from "./pages/channel";
+import Upload from "./pages/upload";
 import SignIn from "./pages/sign-in";
 import SignUp from "./pages/sign-up";
+import Profile from "./pages/profile";
+import Settings from "./pages/settings";
+import Payments from "./pages/payments";
+import Banking from "./pages/banking";
 import ScrollToTop from "./components/scroll-to-top";
 
 const Home = lazy(() => import("./pages/home"));
@@ -27,6 +32,11 @@ function App() {
                 <Route exact path={ROUTES.HOME} component={Home} />
                 <Route exact path={ROUTES.VIDEO} component={Video} />
                 <Route exact path={ROUTES.CHANNEL} component={Channel} />
+                <Route exact path={ROUTES.UPLOAD} component={Upload} />
+                <Route exact path={ROUTES.PROFILE} component={Profile} />
+                <Route exact path={ROUTES.SETTINGS} component={Settings} />
+                <Route exact path={ROUTES.PAYMENTS} component={Payments} />
+                <Route exact path={ROUTES.BANKING} component={Banking} />
               </Switch>
             </Route>
           </Switch>
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -4,5 +4,9 @@ export const CHANNEL = "/c/:id";
 export const VIDEO = "/v/:id";
 export const SIGN_UP = "/sign-up";
 export const UPLOAD = "/upload";
+export const PROFILE = "/my/profile";
+export const SETTINGS = "/my/settings";
+export const PAYMENTS = "/my/payments";
+export const BANKING = "/my/banking";
 
 export const NOT_FOUND = "/not-found";
