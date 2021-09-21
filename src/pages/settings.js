8f8d62fac7ed8ba73dec70ca8665f9bad4fcc71e
Ali Eskandari <alieskandari3@gmail.com>
2021-09-21 11:10:17 -0700

    Added Settings Page

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,5 +1,62 @@
 import React from "react";
+import { Row, Form, Button, Col, FloatingLabel } from "react-bootstrap";
 
 export default function Settings() {
-  return <div>Settings</div>;
+  return (
+    <Row className="justify-content-center">
+      <Col xs={12} md={6} xl={6} className="border-3">
+        <Form>
+          <div className="mb-3 d-flex justify-content-between align-items-center">
+            <h2 className="mb-0 lh-base">Edit Profile</h2>
+            <Button variant="primary" type="submit">
+              SAVE
+            </Button>
+          </div>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter channel name" />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Bio</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={2}
+              placeholder="Describe your channel"
+            />
+          </Form.Group>
+
+          <Row>
+            <Form.Group as={Col} className="mb-3">
+              <Form.Label>Country</Form.Label>
+              <Form.Control type="text" placeholder="Country" />
+            </Form.Group>
+            <Form.Group as={Col} className="mb-3">
+              <Form.Label>Specialty</Form.Label>
+              <Form.Control type="text" placeholder="Ex. Cardio" />
+            </Form.Group>
+          </Row>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Motivatement</Form.Label>
+            <Form.Control type="text" placeholder="Give it 110%!" />
+          </Form.Group>
+        </Form>
+
+        <hr />
+
+        <Row>
+          <Form.Group as={Col} className="mb-3">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group as={Col} className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+        </Row>
+      </Col>
+    </Row>
+  );
 }
