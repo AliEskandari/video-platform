55b03ab278fffd8c43a3a0f1ee1ae7f44e928c6e
Ali Eskandari <AliEskandari3@gmail.com>
2021-09-25 17:57:51 -0700

    added basic about us

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Col } from "react-bootstrap";
 
 export default function about() {
   return (
     <Container>
-      <h1>About</h1>
+      <Col>
+        <h1>About Us</h1>
+
+        <p>
+          DomumGym believes everyone should be able to have access to
+          high-quality, engaging, and effective fitness content and instructors.
+          That's why we provide over 1 million exercise videos free of charge.
+        </p>
+
+        <p>
+          Content creators work hard to make effective fitness content, that's
+          why we created a way for fitness instructors to be compensated for
+          their work.
+        </p>
+      </Col>
     </Container>
   );
 }
