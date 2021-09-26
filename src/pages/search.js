b88fe4a92a1bebe3e2ac84c83c01d0abfcccc2cb
Ali Eskandari <AliEskandari3@gmail.com>
2021-09-25 17:38:02 -0700

    Added basic search results page

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,6 +1,17 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import VideoCard from "../components/video-card";
 
 export default function search() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Row xs={1} sm={2} md={2} lg={3} xl={5} className="g-4">
+        {Array.from({ length: 40 }).map((_, idx) => (
+          <Col key={idx}>
+            <VideoCard />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
