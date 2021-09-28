d81d71fe7660528d699f206e70edba17c230c100
Ali Eskandari <AliEskandari3@gmail.com>
2021-09-27 19:13:43 -0700

    sign in with google and email

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,10 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 import * as ROUTES from "../constants/routes";
 
+// Firebase
+import FirebaseContext from "../context/firebase";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
 export default function SignIn() {
   const history = useHistory();
+  const provider = new GoogleAuthProvider();
+  const { app } = useContext(FirebaseContext);
+  const auth = getAuth(app);
 
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
@@ -14,8 +26,21 @@ export default function SignIn() {
 
   const handleSignIn = async (event) => {
     event.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, emailAddress, password);
+      history.push(ROUTES.HOME);
+    } catch (error) {
+      setEmailAddress("");
+      setPassword("");
+      setError(error.message);
+    }
+  };
+
+  const handleGoogleSignIn = async (event) => {
+    event.preventDefault();
 
     try {
+      await signInWithPopup(auth, provider);
       history.push(ROUTES.HOME);
     } catch (error) {
       setEmailAddress("");
@@ -37,18 +62,33 @@ export default function SignIn() {
             Hit the gym with the click of a mouse
           </h2>
           <p className="">Sign in</p>
-          <Form onSubmit={handleSignIn}>
+
+          {error && <p className="mb-4 text-danger">{error}</p>}
+
+          <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Control type="email" placeholder="Email" />
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                onChange={({ target }) => setEmailAddress(target.value)}
+                value={emailAddress}
+              />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={({ target }) => setPassword(target.value)}
+                value={password}
+              />
             </Form.Group>
             <Button
               size="md"
               className="w-100 mb-4"
               variant="primary"
               type="submit"
+              disabled={isInvalid}
+              onClick={handleSignIn}
             >
               SIGN IN
             </Button>
@@ -62,6 +102,7 @@ export default function SignIn() {
               className="w-100"
               variant="secondary"
               type="submit"
+              onClick={handleGoogleSignIn}
             >
               SIGN IN WITH GOOGLE
             </Button>
