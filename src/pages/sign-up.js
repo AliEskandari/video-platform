49c1c7c592336cc77f9dc5df36c1b431bb74fdf3
Ali Eskandari <AliEskandari3@gmail.com>
2021-09-27 19:32:50 -0700

    signup with google

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -3,21 +3,86 @@ import { Link, useHistory } from "react-router-dom";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import * as ROUTES from "../constants/routes";
 
+// Firebase
+import FirebaseContext from "../context/firebase";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 export default function SignIn() {
   const history = useHistory();
 
+  const { app } = useContext(FirebaseContext);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+  const provider = new GoogleAuthProvider();
+
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
+  const [fullName, setFullName] = useState("");
 
   const [error, setError] = useState("");
   const isInvalid = password === "" || emailAddress === "";
 
-  const handleSignIn = async (event) => {
+  const handleSignUp = async (event) => {
     event.preventDefault();
 
     try {
+      const createdUserResult = await createUserWithEmailAndPassword(
+        auth,
+        emailAddress,
+        password
+      );
+
+      // authentication
+      // -> emailAddress & password & full name (displayName)
+      await updateProfile(createdUserResult.user, {
+        displayName: fullName,
+      });
+
+      // firebase user collection (create a document)
+      await addDoc(collection(db, "users"), {
+        userId: createdUserResult.user.uid,
+        fullName,
+        emailAddress: emailAddress.toLowerCase(),
+        following: [],
+        followers: [],
+        dateCreated: Date.now(),
+      });
+
       history.push(ROUTES.HOME);
     } catch (error) {
+      setFullName("");
+      setEmailAddress("");
+      setPassword("");
+      setError(error.message);
+    }
+  };
+
+  const handleGoogleSignUp = async (event) => {
+    event.preventDefault();
+
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // firebase user collection (create a document)
+      await addDoc(collection(db, "users"), {
+        userId: user.uid,
+        fullName: user.displayName,
+        emailAddress: user.email.toLowerCase(),
+        following: [],
+        followers: [],
+        dateCreated: Date.now(),
+      });
+
+      history.push(ROUTES.HOME);
+    } catch (error) {
+      setFullName("");
       setEmailAddress("");
       setPassword("");
       setError(error.message);
@@ -25,7 +90,7 @@ export default function SignIn() {
   };
 
   useEffect(() => {
-    document.title = "Sign In - DomumGym";
+    document.title = "Sign Up - DomumGym";
   }, []);
 
   return (
@@ -37,21 +102,39 @@ export default function SignIn() {
             Hit the gym with the click of a mouse
           </h2>
           <p className="">Create an account</p>
-          <Form onSubmit={handleSignIn}>
+          <Form>
+            {error && <p className="mb-4 text-danger">{error}</p>}
             <Form.Group className="mb-3">
-              <Form.Control type="email" placeholder="Email" />
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                onChange={({ target }) => setEmailAddress(target.value)}
+                value={emailAddress}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={({ target }) => setPassword(target.value)}
+                value={password}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
-              <Form.Control type="text" placeholder="Name" />
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                onChange={({ target }) => setFullName(target.value)}
+                value={fullName}
+              />
             </Form.Group>
             <Button
               size="md"
               className="w-100 mb-4"
               variant="primary"
               type="submit"
+              disabled={isInvalid}
+              onClick={handleSignUp}
             >
               SIGN UP
             </Button>
@@ -67,6 +150,7 @@ export default function SignIn() {
               className="w-100"
               variant="secondary"
               type="submit"
+              onClick={handleGoogleSignUp}
             >
               SIGN IN WITH GOOGLE
             </Button>
---
+++ src/services/firebase.js
@@ -0,0 +1,11 @@
+import { firebase } from "../lib/firebase";
+
+export async function doesUsernameExist(username) {
+  const result = await firebase
+    .firestore()
+    .collection("users")
+    .where("username", "==", username.toLowerCase())
+    .get();
+
+  return result.docs.length > 0;
+}
