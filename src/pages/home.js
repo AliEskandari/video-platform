af2745d78f4d6dc3cbd721a7003eb69277dec5e2
Ali Eskandari <AliEskandari3@gmail.com>
2021-10-02 11:25:48 -0700

    fetch all vids on home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import VideoCard from "../components/video-card";
+import useVideos from "../hooks/use-videos";
 export default function Home() {
+  const { videos } = useVideos({ all: true });
   return (
     <Container>
       <Row xs={1} sm={2} md={2} lg={3} xl={5} className="g-2">
-        {Array.from({ length: 40 }).map((_, idx) => (
+        {videos.map((video, idx) => (
           <Col key={idx}>
-            <VideoCard />
+            <VideoCard video={video} />
           </Col>
         ))}
+        {!videos.length &&
+          Array.from({ length: 40 }).map((_, idx) => (
+            <Col key={idx}>
+              <VideoCard />
+            </Col>
+          ))}
       </Row>
     </Container>
   );
