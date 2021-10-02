14001e10c9823fb2ab206c1d0305e4c6fba82988
Ali Eskandari <AliEskandari3@gmail.com>
2021-10-02 14:27:35 -0700

    pull settings data from backend

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,10 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Container, Row, Form, Button, Col } from "react-bootstrap";
 import UserContext from "../context/user";
+import useUser from "../hooks/use-user";
 import { deleteUser } from "../services/firebase";
 
 export default function Settings() {
   const { user: authUser } = useContext(UserContext);
+  const { user } = useUser(authUser.uid);
+
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [speciality, setSpeciality] = useState("");
+  const [country, setCountry] = useState("");
+  const [motivatement, setMotivatement] = useState("");
+
+  useEffect(() => {
+    if (user) {
+      // pre-fill values for form
+      setName(user.name);
+      setBio(user.bio);
+      setSpeciality(user.speciality);
+      setCountry(user.country);
+      setMotivatement(user.motivatement);
+    }
+  }, [user]);
   const handleDeleteAccount = () => {
     deleteUser(authUser);
   };
@@ -21,7 +40,11 @@ export default function Settings() {
             </div>
             <Form.Group className="mb-3">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter channel name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter channel name"
+                value={name}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Bio</Form.Label>
@@ -29,21 +52,34 @@ export default function Settings() {
                 as="textarea"
                 rows={3}
                 placeholder="Describe your channel"
+                value={bio}
               />
             </Form.Group>
             <Row>
               <Form.Group as={Col} className="mb-3">
                 <Form.Label>Country</Form.Label>
-                <Form.Control type="text" placeholder="Country" />
+                <Form.Control
+                  type="text"
+                  placeholder="Country"
+                  value={country}
+                />
               </Form.Group>
               <Form.Group as={Col} className="mb-3">
                 <Form.Label>Specialty</Form.Label>
-                <Form.Control type="text" placeholder="Ex. Cardio" />
+                <Form.Control
+                  type="text"
+                  placeholder="Ex. Cardio"
+                  value={speciality}
+                />
               </Form.Group>
             </Row>
             <Form.Group className="pb-3">
               <Form.Label>Motivatement</Form.Label>
-              <Form.Control type="text" placeholder="Give it 110%!" />
+              <Form.Control
+                type="text"
+                placeholder="Give it 110%!"
+                value={motivatement}
+              />
             </Form.Group>
           </Form>
           <hr />
