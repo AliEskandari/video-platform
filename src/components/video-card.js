27c060a61dd38a6044eff7ae5a8b568a066864b9
Ali Eskandari <AliEskandari3@gmail.com>
2021-10-04 21:24:12 -0700

    video card can hide edit button & uniform thumnail

--- src/components/video-card.js
+++ src/components/video-card.js
@@ -6,28 +6,40 @@ import Skeleton from "react-loading-skeleton";
 import { formatDistanceToNow } from "date-fns";
 import "./video-card.css";
 
-export default function VideoCard({ video, showUserName = true }) {
+export default function VideoCard({
+  video,
+  showUserName = true,
+  showEditButton = false,
+}) {
   return (
     <Card className="border-0">
       <Link className="card-img" to={ROUTES.VIDEO.replace(":id", video?.docId)}>
-        {video?.thumbUrl ? (
-          <Card.Img variant="top" src={video.thumbUrl} />
-        ) : (
-          <Skeleton
-            className="img card-img-top img-fluid"
-            width="100%"
-            style={{ paddingTop: "56.25%" }}
-          />
-        )}
+        <div className="ratio ratio-16x9">
+          {video?.thumbUrl ? (
+            <Card.Img
+              variant="top"
+              style={{ objectFit: "cover" }}
+              src={video.thumbUrl}
+            />
+          ) : (
+            <Skeleton
+              className="img card-img-top lh-base"
+              height="100%"
+              width="100%"
+            />
+          )}
+        </div>
       </Link>
-      <div className="card-img-overlay position-absolute w-100 h-100 pe-none">
-        <Link
-          to={ROUTES.EDIT_VIDEO.replace(":id", video?.docId)}
-          className="edit-button float-end pe-auto d-none text-white py-1 px-2 bg-black bg-opacity-50 rounded-2"
-        >
-          <i className="bi bi-pencil-fill"></i>
-        </Link>
-      </div>
+      {showEditButton && (
+        <div className="card-img-overlay position-absolute w-100 h-100 pe-none">
+          <Link
+            to={ROUTES.EDIT_VIDEO.replace(":id", video?.docId)}
+            className="edit-button float-end pe-auto d-none text-white py-1 px-2 bg-black bg-opacity-50 rounded-2"
+          >
+            <i className="bi bi-pencil-fill"></i>
+          </Link>
+        </div>
+      )}
       <Card.Body className="px-0">
         <Card.Title className="text-truncate fs-6">
           <Link
