d6f6deb64bc6f3997b75f7204cb04f2f42f58297
Ali Eskandari <AliEskandari3@gmail.com>
2021-10-05 20:00:10 -0700

    created submit button component

---
+++ src/components/subscribe-button.js
@@ -0,0 +1,48 @@
+import React, { useContext, useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import ModalContext from "../context/modal";
+import UserContext from "../context/user";
+import useSubscriptions from "../hooks/use-subscriptions";
+import { subscribeToUser, unsubcribeFromUser } from "../services/firebase";
+
+export default function SubscribeButton({
+  channelUserId,
+  isSubscribed,
+  setIsSubscribed,
+}) {
+  const { user: loggedInUser } = useContext(UserContext);
+  const { showModal } = useContext(ModalContext);
+
+  const handleClick = (event) => {
+    const subscribe = () => {
+      const handleProceed = async () => {
+        await subscribeToUser(loggedInUser.uid, channelUserId);
+        setIsSubscribed(true);
+      };
+      showModal({ text: "Subscribe for 4.99?", handleProceed });
+    };
+
+    const unsubscribe = () => {
+      const handleProceed = async () => {
+        await unsubcribeFromUser(loggedInUser.uid, channelUserId);
+        setIsSubscribed(false);
+      };
+      showModal({
+        text: "Are you sure you want to unsubscribe?",
+        handleProceed,
+      });
+    };
+    isSubscribed ? unsubscribe() : subscribe();
+  };
+
+  return (
+    <Button
+      variant={isSubscribed ? "outline-primary" : "primary"}
+      className=""
+      size=""
+      onClick={handleClick}
+    >
+      {isSubscribed ? "Subscribed" : "Subscribe"}
+    </Button>
+  );
+}
