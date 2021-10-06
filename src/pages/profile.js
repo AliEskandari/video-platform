29d32eab460e0d64ec48f7c00910e6790e701362
Ali Eskandari <AliEskandari3@gmail.com>
2021-10-05 20:02:36 -0700

    profile always displays vid images

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -66,6 +66,7 @@ export default function Profile() {
                   video={video}
                   showUserName={false}
                   showEditButton={true}
+                  isSubscribed={true}
                 />
               </Col>
             ))}
